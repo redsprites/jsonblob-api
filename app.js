7cb8d7637128537e55ad6d6602f50ad4780c508d
redsprites <71545773+redsprites@users.noreply.github.com>
2023-04-02 19:47:59 -0400

    modified .rest, put, post and delete

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const http=require('http');
-const server=http.createServer().listen(8080);
 const GET =require('./lib/get.js');
 const POST=require('./lib/post.js');
 const PUT=require('./lib/put.js');
 const DELETE=require('./lib/delete.js');
+const http=require('http');
+const server=http.createServer((req,res)=>{
+	  // Set CORS headers
+	  res.setHeader('Access-Control-Allow-Origin', '*');
+	  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+});
+ var port = 8080
+server.listen(port, () => {
+	console.log('Server started on port', port);
+  });
 
 server.on('request',async(req,res)=>{
 	console.log(req.method)
